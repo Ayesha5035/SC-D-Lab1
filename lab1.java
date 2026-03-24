fd03a3537f1045b8809f9469328fc35b3ee64807
Ayesha <muhammadmohid621@gmail.com>
2026-03-24 14:41:07 +0500

    Updated version

--- lab1.java
+++ lab1.java
@@ -1,5 +1,5 @@
 public class lab1 {
     public static void main(String[] args){
-        System.out.println("This is first java code of Ayesha Farooq");
+        System.out.println("This is the updated version of the first Java code.");
     }
 }
